3bf1f9b5dc8793a12bb86f4be513f7defb3f5658
Masterfar7 <145840792+Masterfar7@users.noreply.github.com>
2025-11-29 10:58:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/multipage-api-app-react/', 
+  base: '/multipage-api-app-react/',
+  build: {
+    outDir: 'dist'
+  }
 });
